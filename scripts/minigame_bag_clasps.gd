2a98014bf0ca14a06b5e054fb4ca864713b1d5d4
Joe Forster <imekke@gmail.com>
2025-08-07 22:22:41 +0100

    HACK CITY: get consistent scanner gameplay for the clasps bag type too (with a ton of code duplication for now) rename the minigame to indicate it's the clasp game (they're all physics based now, except the old zip one we don't have used)

--- scripts/minigame_bag_clasps.gd
+++ scripts/minigame_bag_clasps.gd
@@ -3,12 +3,17 @@ extends MiniGameBase
 @export var open_speed = 100.0
 @export var open_dist = 240.0
 @export var complete_time = 1.0
+@export var light_time = 1.0
 @export var clasps : Array[BagClasp]
 @export var contents_holder : Node2D
 @export var item_spawn_line : Path2D
 @export var tray_area : Area2D
+@export var repack_button : Button
+@export var repack_indicator : Circle2D
+@export var item_scanner : ItemScanner
 
 var complete_timer = -1.0
+var light_timer = -1.0
 
 func init_with_contents(game_rules : GameRules, new_bag_contents : BagContents):
 	super(game_rules, new_bag_contents)
@@ -30,7 +35,7 @@ func _on_clasp_input_event(_viewport: Node, event: InputEvent, _shape_idx: int, 
 
 # TODO replace this mess with a state machine
 var forbidden_items_for_tray : Array[ScannedShape]
-var spawned_items = false
+var spawned_all_items = false
 
 func _process(delta: float) -> void:
 	if complete_timer > 0.0:
@@ -44,7 +49,7 @@ func _process(delta: float) -> void:
 				has_unopened_clasps = true
 				break
 				
-		if !has_unopened_clasps and !spawned_items:
+		if !has_unopened_clasps and !spawned_all_items:
 			# take all the shapes out of the bag and spawn them so they'll fall
 			for show_row : Node2D in contents_holder.get_child(0).get_rows():
 				for show_shape : ScannedShape in show_row.get_children():
@@ -60,17 +65,34 @@ func _process(delta: float) -> void:
 					var spawn_point = item_spawn_line.curve.sample(point_index, point_interp)
 					var spawn_point_global = item_spawn_line.to_global(spawn_point)
 					show_shape.set_global_position(spawn_point_global)
-					if rules.shape_breaks_rule(show_shape):
+					if show_shape.real_appearance:
+						item_scanner.items_needing_scan.push_back(show_shape)
+					if rules.shape_breaks_rule(show_shape) and (!show_shape.real_appearance or !show_shape.is_legit):
 						forbidden_items_for_tray.push_back(show_shape)
-			spawned_items = true
+			spawned_all_items = true
 			
-		if spawned_items && complete_timer == -1.0:
-			# Assumes only items can overlap with the area (should be set in layers)
-			if tray_area.get_overlapping_bodies().size() == forbidden_items_for_tray.size():
-				var all_correct_items_in_tray = true
-				for body in forbidden_items_for_tray:
-					if body not in tray_area.get_overlapping_bodies():
-						all_correct_items_in_tray = false
-						break
-				if all_correct_items_in_tray:
-					complete_timer = complete_time
+	# #3 CHECKING ITEMS STATE - wait for forbidden items processing to be satisfied 
+	var valid_repack = false
+	if spawned_all_items && complete_timer == -1.0:
+		# Assumes only items can overlap with the area (should be set in layers)
+		# also only allow us to repack if we've scanned all the items that need it
+		if item_scanner.items_needing_scan.is_empty() and tray_area.get_overlapping_bodies().size() == forbidden_items_for_tray.size():
+			valid_repack = true
+			for body in forbidden_items_for_tray:
+				if body not in tray_area.get_overlapping_bodies():
+					valid_repack = false
+					break
+	
+		if light_timer > 0.0:
+			light_timer -= delta
+			if light_timer <= 0.0:
+				light_timer = 0.0
+				repack_indicator.set_color(Color.BLACK)
+		
+		if repack_button.is_pressed():
+			if valid_repack:
+				complete_timer = complete_time
+				repack_indicator.set_color(Color.GREEN)
+			else:
+				repack_indicator.set_color(Color.RED)
+			light_timer = light_time
