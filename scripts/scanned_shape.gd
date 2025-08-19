0d4a1b628917e107b7d154a1206ac7b0ac0d5d3a
Joe Forster <imekke@gmail.com>
2025-08-19 21:45:23 +0100

    Tweaks to the physics minigame to rotate the item upright, and some disabled experimental code to improve the way the object moves.

--- scripts/scanned_shape.gd
+++ scripts/scanned_shape.gd
@@ -11,11 +11,25 @@ extends RigidBody2D
 @export var bag_section_id = 0
 @export var is_legit = true
 
+@export var rotate_upright_rate = PI * 1.5
+
 # NOTE these won't be copied when added to a minigame via clone so should only be runtime values!
 var colour : Color
 var colour_name : String
 var mouse_dragging = false
 var physics_enabled = false
+
+# Internal hard-code settings
+
+enum ITEM_DRAG_MODE
+{
+	FORCE_POS,
+	THRUST,
+	MOVE_AND_COLLIDE
+}
+
+var item_drag_mode = ITEM_DRAG_MODE.FORCE_POS
+var move_and_collide_speed = 100.0
 
 func enable_physics():
 	if !physics_enabled:
@@ -65,10 +79,58 @@ func _input(event: InputEvent) -> void:
 	var click_event = event as InputEventMouseButton
 	if click_event and click_event.button_index == 1 && !click_event.pressed:
 		mouse_dragging = false
+
+func _physics_process_pickup_force_pos(delta: float) -> void:
+	freeze = mouse_dragging
+	if mouse_dragging:
+		var drag_offset = get_global_position() - $DragPoint.get_global_position()
+		set_global_position(get_global_mouse_position() + drag_offset)
+		var rotate_rate = rotate_upright_rate * delta
+		var rotate_diff = 0.0 - rotation
+		rotate(rotate_diff if rotate_diff < rotate_rate else rotate_rate)
+	
+func _physics_process_pickup_thrust(delta: float) -> void:
+	if mouse_dragging:
+		# TODO this can be simplified
+		gravity_scale = 0.0
+		var drag_offset = get_global_position() - $DragPoint.get_global_position()
+		var target_position_global = get_global_mouse_position() + drag_offset
+		var to_target_position : Vector2 = target_position_global - get_global_position()
+		# TODO for stability we need to do some maths here to figure out the ideal force once close to the target so it doesn't overshoot
+		if to_target_position.length() > 100.0:
+			apply_force(to_target_position.normalized() * 5000.0)
+	else:
+		gravity_scale = 1.0
+
+func _physics_process_pickup_move_and_collide(delta: float) -> void:
+	if mouse_dragging:
+		# TODO this can be simplified
+		gravity_scale = 0.0
+		var drag_offset = get_global_position() - $DragPoint.get_global_position()
+		var target_position_global = get_global_mouse_position() + drag_offset
+		var to_target_position : Vector2 = target_position_global - get_global_position()
 		
-func _physics_process(_delta: float) -> void:
+		# TODO for stability we need to do some maths here to figure out the ideal force once close to the target so it doesn't overshoot
+		
+		var move_vector = to_target_position
+		var move_amount_max = move_and_collide_speed * delta
+		if move_vector.length() > move_amount_max:
+			move_vector = move_vector.normalized() * move_amount_max
+		move_and_collide(move_vector)
+			
+			
+	else:
+		gravity_scale = 1.0
+
+func _physics_process(delta: float) -> void:
+	# Method 1: freeze and force it to the mouse position.
+	# TODO slowly rotate to upright
 	if physics_enabled:
-		freeze = mouse_dragging
-		if mouse_dragging:
-			var drag_offset = get_global_position() - $DragPoint.get_global_position()
-			set_global_position(get_global_mouse_position() + drag_offset)
+		if item_drag_mode == ITEM_DRAG_MODE.FORCE_POS:
+			_physics_process_pickup_force_pos(delta)
+		elif item_drag_mode == ITEM_DRAG_MODE.THRUST:
+			_physics_process_pickup_thrust(delta)
+		elif item_drag_mode == ITEM_DRAG_MODE.MOVE_AND_COLLIDE:
+			_physics_process_pickup_move_and_collide(delta)
+
+			
